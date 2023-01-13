c0319cc8f08c9c5269cdde43f54fb36120c4c287
Ida Christine Krokeide <ida.krokeide@sparebank1.no>
2023-01-13 07:45:56 +0100

    Week 2

--- control-flow-strings/src/main.rs
+++ control-flow-strings/src/main.rs
@@ -27,7 +27,7 @@ fn main() {
             double()
         } else {
             count(arg)
-        };
+        }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -54,6 +54,11 @@ fn double() {
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
+    while x < 500 {
+        x *= 2;
+        count += 1;
+    }
+
     println!(
         "You can double x {} times until x is larger than 500",
         count
@@ -61,10 +66,18 @@ fn double() {
 }
 
 fn count(arg: String) {
+    let mut count = 0;
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+    loop {
+        if count == 8 {
+            break;
+        } 
+        print!("{} ", arg);
+        count += 1;
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
